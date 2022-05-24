f2a2ae0869af4f23251ce97c681aba313e62e59c
84311 <email>
2022-05-24 20:36:37 +0200

    Fixed not working CSS and added @Param annotation in CodeRepository.

--- Code Sharing Platform/task/src/platform/CodeRepository.java
+++ Code Sharing Platform/task/src/platform/CodeRepository.java
@@ -2,6 +2,7 @@ package platform;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,5 +11,5 @@ public interface CodeRepository extends JpaRepository<Code, String> {
             value = "SELECT * FROM code c " +
                     "WHERE c.time_restricted = FALSE AND c.view_restricted = FALSE " +
                     "ORDER BY c.date_time DESC LIMIT :limit")
-    List<Code> findLastNAdded(long limit);
+    List<Code> findLastNAdded(@Param("limit") long limit);
 }
